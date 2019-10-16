be21a1f3d6b5ecfb98a6ec5042f4a3d8005e786e
jerin rinika <jerinrinika@jerins-MacBook-Pro.local>
2019-10-15 21:48:35 -0400

    Ergast Motor Racing Module has been added

---
+++ ErgastMotorRacing/src/test/java/RaceSchedule.java
@@ -0,0 +1,19 @@
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class RaceSchedule {
+    public String url= "http://ergast.com/api/f1/current.json";
+    @Test
+    public void getRaceScheduleForCurrentSeason(){
+        Response response= given().when().get(url).then().statusCode(200).extract().response();
+        String statusLine = response.getStatusLine();
+        int statusCode = response.getStatusCode();
+        String body = response.getBody().prettyPrint();
+        Assert.assertEquals(statusLine, "HTTP/1.1 200 OK");
+        Assert.assertEquals(statusCode, 200);
+    }
+}
--- Generic/src/main/java/databases/ConnectToSqlDB.java
+++ Generic/src/main/java/databases/ConnectToSqlDB.java
@@ -145,7 +145,7 @@ public class ConnectToSqlDB {
             ps.executeUpdate();
             ps = connect.prepareStatement("CREATE TABLE `"+tableName+"` (`ID` int(11) NOT NULL AUTO_INCREMENT,`SortingNumbers` bigint(20) DEFAULT NULL,  PRIMARY KEY (`ID`) );");
             ps.executeUpdate();
-            for(String st:list){
+            for(Student st:list){
                 ps = connect.prepareStatement("INSERT INTO "+tableName+" ( "+columnName+" ) VALUES(?)");
                 ps.setObject(1,st);
                 ps.executeUpdate();
